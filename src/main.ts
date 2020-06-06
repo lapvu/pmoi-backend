2e2839d0ac6c1b328367e016b8f767fa57afc6e4
lapvu <lapvv62@wru.vn>
2020-06-06 17:28:54 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: "*",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
